e89fa826cf29624a532e34bc1671ae45f7d75142
CJ <lim@chernjie.com>
2015-10-19 19:22:34 +0800

    Initial commit of image extractor

---
+++ bin/extractimages.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+_require () {
+  for i
+  do
+    if ! command -v "$i" > /dev/null
+    then
+      echo "Command not found $i" >&2
+      exit 1
+    fi
+  done
+}
+
+_downloadImages() {
+  local _dest1
+  while read i
+  do
+    _dest1=tmp/"$(echo $i | md5)"
+    curl "$i" --output "$_dest1" --silent --location
+    _dest2=images/$(_generateFilename "$_dest1")
+    mv -v "$_dest1" "$_dest2"
+    echo "$_dest2" "$i" >> images.map
+  done
+}
+
+_generateFilename () {
+  _encodeFileName () {
+    md5 < "$1" # | cut -d= -f2- | tr -d ' '
+  }
+  _findFileExtention () {
+    file "$1" | cut -d: -f2- | cut -d\  -f2 | tr '[:upper:]' '[:lower:]'
+  }
+  echo $(_encodeFileName "$1").$(_findFileExtention "$1")
+}
+
+_require xml2 md5 file cut curl mv grep xargs
+
+mkdir -p images tmp
+rm images.map
+rm images/*
+xml2 < *.kml  | grep /kml/Document/Folder/Placemark/ExtendedData/Data/value | cut -d= -f2- | xargs -n1 | _downloadImages
